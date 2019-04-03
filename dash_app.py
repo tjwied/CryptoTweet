f4eda109a3eb7b1d4f6385566709c66347b0fd2e
Tyler Wied <tjwied@gmail.com>
2019-04-02 20:49:11 -0700

    Improved readability for dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -23,6 +23,8 @@ import os
 csv_list = glob.glob("/home/rubisco/Desktop/insight/dash/final/csv/*.csv")
 cwd = os.getcwd()
 
+
+# Initialize lists that will be used downstream
 G_list = []
 edge_trace_list = []
 node_trace_list = []
@@ -30,14 +32,15 @@ topic_list = []
 node_lengths = []
 edge_lengths = []
 coin_list = []
+
+
+# Initialize all information for all graphs to be visualized
 for element in csv_list:
     basename_extract = os.path.basename(element[:-4])
     coin_list.append(basename_extract)
-    #build_graph.build_graph(element)
     nlp = graph_nlp.topic_modeling(element)
     betweenness = pickle.load( open( basename_extract+"_between.pkl", "rb" ))
     ranking = sorted(betweenness, key=betweenness.get, reverse=True)
-    #build_graph.build_graph(element)
     graph_components = modify_graph.plot_graph(cwd+'/'+basename_extract+'.grid')
     G_list.append(graph_components[0])
     edge_trace_list.append(graph_components[1])
@@ -56,6 +59,16 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+"""
+
+Set Layout for Dash App. 
+
+Networks are visualized at the top of the page with a slider that allows users to transition between networks
+Below the visualization at the top, there are four columns: 1. Overall data (total nodes and edges),
+2. Top Accounts associated with the network, 3. Top topics in network, and 4. Selected users, which
+can be selected with dragging mouse over nodes.
+
+"""
 app.layout = html.Div([
     dcc.Graph(id='graph-with-slider'),
     dcc.Slider(
@@ -64,7 +77,6 @@ app.layout = html.Div([
         max=len(csv_list),
         value=0,
         marks=marker
-#add to layout here that there should be tex
     ),
         html.Div(className='row', children=[
             html.Div([html.H2('Overall Data'),
@@ -84,11 +96,15 @@ app.layout = html.Div([
                 ])
             ])
 
+
+# Incorporate components that will change when certain options are selected.
+
+# Change displayed network when slider value is changed
+
 @app.callback(
     dash.dependencies.Output('graph-with-slider', 'figure'),
     [dash.dependencies.Input('year-slider', 'value')])
 def update_graph1(selected_year):
-    #filtered_df = df[df.year == selected_year]
     fig    = go.Figure(data=[edge_trace_list[selected_year], node_trace_list[selected_year]],
                 layout=go.Layout(
                 title='<br>Network for '+str(basename_extract),
@@ -108,6 +124,8 @@ def update_graph1(selected_year):
             'data': fig,
         }
 
+# Change Data Summary when year slider is changed
+
 @app.callback(
     Output('data-summary', 'children'),
     [Input('year-slider','value')])
@@ -118,23 +136,27 @@ def display_selected_data(selected_year):
     return summary
 
 
-
+# Change top accounts when slider is moved
+cutoff = 20 # Change this parameter to change number of users displayed
 @app.callback(
     Output('selected-data', 'children'),
     [Input('year-slider','value')])
+
 def display_selected_data(selected_year):
     j = 0
     top_ten = []
     for element in ranking:
-        if j < 20:
+        if j < cutoff:  
             account = html.P(str(element))
             top_ten.append(account)
-        elif j == 20:
+        elif j == cutoff:
             break
         j += 1
     rank = top_ten
     return rank
 
+
+# Change topics displayed when slider is moved
 @app.callback(
     Output('selecteder-data', 'children'),
     [Input('year-slider','value')])
@@ -149,6 +171,7 @@ def display_selected_data(selected_year):
     text = topics
     return text
 
+# Display selected accounts
 @app.callback(
     Output('selected-accounts', 'children'),
     [Input('graph-with-slider','selectedData')])
